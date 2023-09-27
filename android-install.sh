1de7393f0185a82a2e180651d9664bb902ff51fd
pythoninthegrass <4097471+pythoninthegrass@users.noreply.github.com>
2023-09-27 18:36:33 -0500

    Add param 1 for filename, comment out su

--- android-install.sh
+++ android-install.sh
@@ -5,19 +5,19 @@ set -uo pipefail
 
 LOCAL_DIR=$(dirname $0)
 OUTPUT_DIR="${LOCAL_DIR}/static-cross-bins/output/arm-linux-musleabi/bin"
-BBNAME="busybox"
-LOCALBB="${OUTPUT_DIR}/${BBNAME}"
+FN="${1:-busybox}"
+LOCAL_FN="${OUTPUT_DIR}/${FN}"
 SCRIPT='android-remote-install.sh'
 # /data is preferred over /sdcard because it will allow us to execute BB
 TMP="/data"
-TMPBB="${TMP}/busybox"
+TMP_FN="${TMP}/${FN}"
 TGT="/system/bin"
-TGTBB="${TGT}/busybox"
+TGT_FN="${TGT}/${FN}"
 
 # compile busybox if not present
-if [[ ! -f "$LOCALBB" ]]; then
+if [[ ! -f "$LOCAL_FN" ]]; then
 	(cd "${LOCAL_DIR}/static-cross-bins"; \
-	./docker_build.sh TARGET=arm-linux-musleabi busybox; \
+	./docker_build.sh TARGET=arm-linux-musleabi $FN; \
 	cd -)
 fi
 
@@ -27,31 +27,31 @@ main() {
 	adb shell mount | grep "\bsystem\b" | grep "\brw\b"
 	# this is a remount form that works on "partially rooted devices"
 	if [ $? -ne 0 ]; then
-		adb push "$LOCALBB" "$TMPBB"
+		adb push "$LOCAL_FN" "$TMP_FN"
 		adb shell <<-EOF
-		su
+		# su 				# not present on all devices (uncomment if needed)
 		mount -oremount,rw /system
-		"$TMPBB" mount -oremount,rw /system
-		"$TMPBB" rm "$TMPBB"
+		"$TMP_FN" mount -oremount,rw /system
+		"$TMP_FN" rm "$TMP_FN"
 		exit
 		exit
 		EOF
 	fi
 
 	# we should be mounted r/w, push BB
-	adb push "$LOCALBB" "$TGTBB"
+	adb push "$LOCAL_FN" "$TGT_FN"
 
 	# if push fails, try to upload to /sdcard and copy from there
 	if [ $? -ne 0 ]; then
-		adb push "$LOCALBB" "$TMPBB"
-		adb push "$LOCALBB" /mnt/SDCARD/
+		adb push "$LOCAL_FN" "$TMP_FN"
+		adb push "$LOCAL_FN" /mnt/SDCARD/
 		adb shell <<-EOF
-		su
-		cp "/mnt/SDCARD/${BBNAME}" "$TGTBB"
-		chmod 755 "$TGTBB"
-		rm "/mnt/SDCARD/${BBNAME}"
-		"$TMPBB" cp "$TMPBB" "$TGTBB"
-		"$TMPBB" rm "$TMPBB"
+		# su 				# not present on all devices (uncomment if needed)
+		cp "/mnt/SDCARD/${FN}" "$TGT_FN"
+		chmod 755 "$TGT_FN"
+		rm "/mnt/SDCARD/${FN}"
+		"$TMP_FN" cp "$TMP_FN" "$TGT_FN"
+		"$TMP_FN" rm "$TMP_FN"
 		exit
 		exit
 		EOF
@@ -60,8 +60,8 @@ main() {
 	# move the files over to an adb writable location
 	adb push "${LOCAL_DIR}/${SCRIPT}" /mnt/SDCARD/
 	adb shell <<-EOF
-	su
-	"$TGTBB" ash "/mnt/SDCARD/${SCRIPT}"
+	# su 				# not present on all devices (uncomment if needed)
+	"$TGT_FN" ash "/mnt/SDCARD/${SCRIPT}"
 	rm "/mnt/SDCARD/${SCRIPT}"
 	sync
 	exit
