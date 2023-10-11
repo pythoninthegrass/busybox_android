29ed39c54438101b64797d3c6915ac290a5564ef
pythoninthegrass <4097471+pythoninthegrass@users.noreply.github.com>
2023-10-10 19:03:57 -0500

    Update android-install.sh
    - Parameterize sdcard location
    - Better error handling
    - Re-enable su
    - Allow for pushing other bins

--- android-install.sh
+++ android-install.sh
@@ -8,16 +8,18 @@ OUTPUT_DIR="${LOCAL_DIR}/static-cross-bins/output/arm-linux-musleabi/bin"
 FN="${1:-busybox}"
 LOCAL_FN="${OUTPUT_DIR}/${FN}"
 SCRIPT='android-remote-install.sh'
-# /data is preferred over /sdcard because it will allow us to execute BB
+# /data is preferred over /sdcard because it will allow us to execute filename
+SD_CARD="/mnt/SDCARD"
 TMP="/data"
 TMP_FN="${TMP}/${FN}"
 TGT="/system/bin"
 TGT_FN="${TGT}/${FN}"
 
-# compile busybox if not present
-if [[ ! -f "$LOCAL_FN" ]]; then
+# TODO: get basename of ./include/*.mak files
+# compile included make builds if not present
+if [[ ! -f "$LOCAL_FN" ]] && [[ "$FN" = "busybox" ]]; then
 	(cd "${LOCAL_DIR}/static-cross-bins"; \
-	./docker_build.sh TARGET=arm-linux-musleabi $FN; \
+	./docker_build.sh TARGET=arm-linux-musleabi "$FN"; \
 	cd -)
 fi
 
@@ -29,7 +31,7 @@ main() {
 	if [ $? -ne 0 ]; then
 		adb push "$LOCAL_FN" "$TMP_FN"
 		adb shell <<-EOF
-		# su 				# not present on all devices (uncomment if needed)
+		su
 		mount -oremount,rw /system
 		"$TMP_FN" mount -oremount,rw /system
 		"$TMP_FN" rm "$TMP_FN"
@@ -44,12 +46,12 @@ main() {
 	# if push fails, try to upload to /sdcard and copy from there
 	if [ $? -ne 0 ]; then
 		adb push "$LOCAL_FN" "$TMP_FN"
-		adb push "$LOCAL_FN" /mnt/SDCARD/
+		adb push "$LOCAL_FN" "$SD_CARD"
 		adb shell <<-EOF
-		# su 				# not present on all devices (uncomment if needed)
-		cp "/mnt/SDCARD/${FN}" "$TGT_FN"
+		su
+		cp "$SD_CARD/${FN}" "$TGT_FN"
 		chmod 755 "$TGT_FN"
-		rm "/mnt/SDCARD/${FN}"
+		rm "${SD_CARD}/${FN}"
 		"$TMP_FN" cp "$TMP_FN" "$TGT_FN"
 		"$TMP_FN" rm "$TMP_FN"
 		exit
@@ -58,15 +60,17 @@ main() {
 	fi
 
 	# move the files over to an adb writable location
-	adb push "${LOCAL_DIR}/${SCRIPT}" /mnt/SDCARD/
-	adb shell <<-EOF
-	# su 				# not present on all devices (uncomment if needed)
-	"$TGT_FN" ash "/mnt/SDCARD/${SCRIPT}"
-	rm "/mnt/SDCARD/${SCRIPT}"
-	sync
-	exit
-	exit
-	EOF
+	adb push "${LOCAL_DIR}/${SCRIPT}" "$SD_CARD"
+	if [ $FN = "busybox" ]; then
+		adb shell <<-EOF
+		su
+		"$TGT_FN" ash "${SD_CARD}/${SCRIPT}"
+		rm "${SD_CARD}/${SCRIPT}"
+		sync
+		exit
+		exit
+		EOF
+	fi
 
 	# needs to be done separately to avoid "device busy" error
 	adb shell mount -o remount,ro /system
